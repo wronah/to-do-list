88f39a036b96433420c89e8c6eceea81d3c2c125
wronah <hubertwro7@gmail.com>
2024-05-22 23:54:33 +0200

    fetch data from supabase and display it as a table

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from './createClient';
+import './App.css'
 
 const App = () => {
 
-  const [ users, setUsers ] = useState([])
+  const [ tasks, setTasks ] = useState([])
 
   useEffect(() => {
-    fetchUsers()
+    fetchTasks()
   }, [])
 
-  async function fetchUsers() {
+  async function fetchTasks() {
     const { data } = await supabase
-    .from('users')
+    .from('tasks')
     .select('*')
-    setUsers(data)
-    console.log(data)
+    setTasks(data)
   }
 
 
   return (
-    <div>App</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>heading</th>
+            <th>description</th>
+            <th>is_done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => 
+            <tr>
+              <td>{task.id}</td>
+              <td>{task.heading}</td>
+              <td>{task.description}</td>
+              <td>{String(task.is_done)}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
